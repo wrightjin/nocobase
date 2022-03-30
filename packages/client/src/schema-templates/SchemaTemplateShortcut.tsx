610dd6a9bab3a25ea846e90c341ba8af13e643e5
Semmy <semmywong@126.com>
2022-03-31 00:35:45 +0800

    fix: high light block menu

--- packages/client/src/schema-templates/SchemaTemplateShortcut.tsx
+++ packages/client/src/schema-templates/SchemaTemplateShortcut.tsx
@@ -1,18 +1,28 @@
 import { LayoutOutlined } from '@ant-design/icons';
-import React from 'react';
+import { useSetState } from 'ahooks';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { PluginManager } from '../plugin-manager';
 
 export const SchemaTemplateShortcut = () => {
   const { t } = useTranslation();
+  const location = useLocation();
+  const [style, setStyle] = useSetState({});
+
+  const pathname = '/admin/block-templates';
+
+  useEffect(() => {
+    setStyle(location.pathname.startsWith(pathname) ? { backgroundColor: '#f18b62' } : { backgroundColor: null });
+  }, [location.pathname]);
   const history = useHistory();
   return (
     <PluginManager.Toolbar.Item
+      style={style}
       icon={<LayoutOutlined />}
       title={t('Block templates')}
       onClick={() => {
-        history.push('/admin/block-templates');
+        history.push(pathname);
       }}
     />
   );
